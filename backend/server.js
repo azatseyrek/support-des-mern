01ad8fa1c2a0a45544c354b5742e1048df0aac92
azatseyrek <azatseyrek@hotmail.com>
2022-09-02 17:34:20 +0300

    routes and controllers start point

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,18 @@
-console.log('server...');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const PORT = process.env.PORT || 8000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).json({
+    message: 'Hello World',
+  });
+});
+
+// Routes
+app.use('/api/users', require('./routes/userRoutes'));
+
+app.use('/api/users/login', require('./routes/userRoutes'));
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
